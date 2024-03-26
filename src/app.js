b687e96812ddfd3d7dbcabc2757a7d1ed85acbc1
Nancy2305 <155437469+Nancy2305@users.noreply.github.com>
2024-03-26 17:33:50 +0000

    added mustache

--- src/app.js
+++ src/app.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const JOBS = require('./job')
+const mustacheExpress = require('mustache-express');
+console.log(JOBS)
+
+
+app.set('views', path.join(__dirname, 'pages'));
+app.set('view engine', 'mustache');
+app.engine('mustache', mustacheExpress());
+
 
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'pages/index.html'));
+   // res.sendFile(path.join(__dirname, 'pages/index.html'));
 })
 
 
